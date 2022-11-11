43358267602d9063d47d8bc9fefe73bf4ca23378
Ibrahim Raimi <raimiibrahim44@gmail.com>
2022-11-11 15:41:26 +0100

    feat: add background animation

--- src/components/ComingSoon.vue
+++ src/components/ComingSoon.vue
@@ -13,6 +13,7 @@ body {
 	font-family: "Satoshi", sans-serif;
 	background-color: #000;
 }
+
 .coming-soon-container {
 	font-size: 1.5rem;
 	color: #fff;
@@ -22,10 +23,31 @@ body {
 	text-align: center;
 	height: 100vh;
 }
+
 .coming-soon {
 	font-size: 9vw;
 	margin: 0;
 	padding: 0;
 	font-weight: 900;
+	--bg-size: 400%;
+	--color-one: hsl(15 90% 55%);
+	--color-two: hsl(40 95% 55%);
+	background: linear-gradient(
+			90deg,
+			var(--color-one),
+			var(--color-two),
+			var(--color-one)
+		)
+		0 0 / var(--bg-size) 100%;
+	color: transparent;
+	-webkit-background-clip: text;
+	background-clip: text;
+	animation: animate-background 8s infinite linear;
+}
+
+@keyframes animate-background {
+	to {
+		background-position: var(--bg-size) 0;
+	}
 }
 </style>
